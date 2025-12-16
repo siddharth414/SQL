9067feb608f29c4856e7747ae6fa6de51b727986
Siddharth Mag <90581558+siddharth414@users.noreply.github.com>
2025-12-16 06:55:47 -0500

    Create Spiral Matrix.py

---
+++ Leetcode DS/Medium/Spiral Matrix.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+
+        res = []
+
+        while matrix:
+
+            res += matrix.pop(0)
+
+            if matrix and matrix[0]:
+
+                for row in matrix:
+                    res.append(row.pop())
+
+            if matrix:
+                res += matrix.pop()[::-1]
+            
+            if matrix and matrix[0]:
+
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+
+        return res           
+        
