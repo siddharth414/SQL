e984be689f66d8af927dd7ae62d462e5577fa79b
Siddharth Mag <90581558+siddharth414@users.noreply.github.com>
2025-12-19 06:47:37 -0500

    Create Best Time to Buy and Sell Stock.py

---
+++ Leetcode DS/Easy/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        l, r = 0, 1
+
+        maxP = 0
+
+        while r < len(prices):
+
+            if prices[l] < prices[r]:
+                maxP = max(maxP,prices[r] - prices[l])
+            
+            else:
+                l = r
+            r+=1
+        return maxP
+
+
+
+
+
+            
+            
+
+
